28d850ee561b050c7de55f6e19b5685bb38222d4
Mateus Bastos Marques <mateus.marques@AGU.GOV.BR>
2025-07-25 16:06:51 -0300

    feat: atualização na pg customerregisterform

--- src/views/customer/CustomerRegisterForm.jsx
+++ src/views/customer/CustomerRegisterForm.jsx
@@ -16,67 +16,69 @@ export default function CustomerRegisterForm() {
         </div>
       </header>
 
-      <div className="w-screen h-screen bg-gray-900 flex justify-center">
-        <Fieldset className="">
-          <Field>
-            <Label className="text-sm/6 font-medium text-white">Name</Label>
-            <Input
-              className={clsx(
-                "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white"
+      <div className="w-auto h-screen bg-gray-900 justify-center">
+        <div className="shadow-sm bg-slate-700 border border-white p-3 ">
+          <Fieldset className="">
+            <Field>
+              <Label className="text-sm/6 font-medium text-white">Name</Label>
+              <Input
+                className={clsx(
+                  "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white"
+                )}
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </Field>
+            <Field>
+              <Label className="text-sm/6 font-medium text-white">
+                Last Name
+              </Label>
+              <Input
+                className={clsx(
+                  "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white"
+                )}
+                type="text"
+                name="lastname"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </Field>
+            <Field>
+              <Label className="text-sm/6 font-medium text-white">CPF</Label>
+              <Input
+                className={clsx(
+                  "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white"
+                )}
+                type="text"
+                name="cpf"
+                inputMode="numeric"
+                maxLength={14}
+                value={getFormattedCpf(formData.cpf)}
+                onChange={handleChange}
+                placeholder="000.000.000-00"
+              />
+            </Field>
+            <Field>
+              <Label className="text-sm/6 font-medium text-white">E-mail</Label>
+              <Input
+                className={clsx(
+                  "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white "
+                )}
+                type="text"
+                name="email"
+                value={formData.email}
+                placeholder="fulano@gmail.com"
+                onChange={handleChangeEmail}
+              />
+              {console.log(formData.error)}
+              {formData.error && (
+                <p className="text-red-500 text-sm">{formData.error}</p>
               )}
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </Field>
-          <Field>
-            <Label className="text-sm/6 font-medium text-white">
-              Last Name
-            </Label>
-            <Input
-              className={clsx(
-                "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white"
-              )}
-              type="text"
-              name="lastname"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
-          </Field>
-          <Field>
-            <Label className="text-sm/6 font-medium text-white">CPF</Label>
-            <Input
-              className={clsx(
-                "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white"
-              )}
-              type="text"
-              name="cpf"
-              inputMode="numeric"
-              maxLength={14}
-              value={getFormattedCpf(formData.cpf)}
-              onChange={handleChange}
-              placeholder="000.000.000-00"
-            />
-          </Field>
-          <Field>
-            <Label className="text-sm/6 font-medium text-white">E-mail</Label>
-            <Input
-              className={clsx(
-                "mt-3 w-full border border-white rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 text-white "
-              )}
-              type="text"
-              name="email"
-              value={formData.email}
-              placeholder="fulano@gmail.com"
-              onChange={handleChangeEmail}
-            />
-            {console.log(formData.error)}
-            {formData.error && (
-              <p className="text-red-500 text-sm">{formData.error}</p>
-            )}
-          </Field>
-        </Fieldset>
+            </Field>
+          </Fieldset>
+        </div>
       </div>
     </>
   );
